592f623f032e704dae4db582a2a6389176645d32
Moulzo <kountamoulzo@gmail.com>
2026-04-08 10:15:20 +0000

    refactor(auth): remove legacy clerk auth pages

--- app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-import { AUTH_ROUTES } from "@/lib/auth-routes";
-
-export default function LegacySignInPage() {
-    redirect(AUTH_ROUTES.login);
-}
--- app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-import { AUTH_ROUTES } from "@/lib/auth-routes";
-
-export default function LegacySignUpPage() {
-    redirect(AUTH_ROUTES.register);
-}
